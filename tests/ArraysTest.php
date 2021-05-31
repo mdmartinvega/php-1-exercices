12fa2fb886dc65307f122b18015147ead313e6d2
mdmartinvega <mdmartinvega@gmail.com>
2021-05-31 14:01:06 +0200

    Empezamos ejercicios de php

--- tests/ArraysTest.php
+++ tests/ArraysTest.php
@@ -5,38 +5,39 @@ use function PHPUnit\Framework\assertEquals;
 
 final class ArraysTest extends TestCase
 {
-    // public function testArrayIndexado1(): void
-    // {
-    //     // Consigue la frase esperada utilizando índices del array y concatenación
-    //     $seasons = ["Primavera", "Verano", "Otoño,", "Invierno"]; 
-    //     $phrase = '';
+    public function testArrayIndexado1(): void
+    {
+        // Consigue la frase esperada utilizando índices del array y concatenación
+        $seasons = ["Primavera", "Verano", "Otoño,", "Invierno"]; 
+        // $phrase = "La estación que más me gusta es el $seasons[1] y la que menos es el $seasons[3]";
+        $phrase = 'La estación que más me gusta es el '.$seasons[1].' y la que menos es el '.$seasons[3];
         
-    //     $expectedPhrase = "La estación que más me gusta es el verano y la que menos es el invierno";
-    //     assertEquals($expectedPhrase, $phrase);
-    // }
+        $expectedPhrase = "La estación que más me gusta es el Verano y la que menos es el Invierno";
+        assertEquals($expectedPhrase, $phrase);
+    }
 
-    // public function testArrayAsociativo(): void
-    // {
-    //     // Consigue el email del usuario utilizando la clave del array
-    //     $user = ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'city' => 'Benalmádena']; 
-    //     $userEmail = '';
+    public function testArrayAsociativo(): void
+    {
+        // Consigue el email del usuario utilizando la clave del array
+        $user = ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'city' => 'Benalmádena']; 
+        $userEmail = $user['email'];
 
-    //     assertEquals('carlos@correo.com', $userEmail);
-    // }
+        assertEquals('carlos@correo.com', $userEmail);
+    }
 
-    // public function testArrayMultidimensional(): void
-    // {
-    //     // Consigue los datos que se piden en los asserts
-    //     $users = [
-    //       ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'city' => 'Benalmádena'],
-    //       ['name' => 'Carmen', 'email' => 'carmen@correo.com', 'city' => 'Fuengirola'],
-    //       ['name' => 'Carmelo', 'email' => 'carmelo@correo.com', 'city' => 'Torremolinos'],
-    //       ['name' => 'Carolina', 'email' => 'carolina@correo.com', 'city' => 'Málaga'],
-    //     ]; 
+    public function testArrayMultidimensional(): void
+    {
+        // Consigue los datos que se piden en los asserts
+        $users = [
+          ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'city' => 'Benalmádena'],
+          ['name' => 'Carmen', 'email' => 'carmen@correo.com', 'city' => 'Fuengirola'],
+          ['name' => 'Carmelo', 'email' => 'carmelo@correo.com', 'city' => 'Torremolinos'],
+          ['name' => 'Carolina', 'email' => 'carolina@correo.com', 'city' => 'Málaga'],
+        ]; 
 
-    //     assertEquals('carlos@correo.com', '');
-    //     assertEquals('Torremolinos', '');
-    //     assertEquals('Carmen', '');
-    // }
+        assertEquals('carlos@correo.com', $users[0]['email']);
+        assertEquals('Torremolinos', $users[2]['city']);
+        assertEquals('Carmen', $users[1]['name']);
+    }
 
 }
